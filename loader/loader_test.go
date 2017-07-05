6e38ebfc913e70604ee555a6b11c99ef8552af8d
gfandada <gfandada@gmail.com>
2017-07-05 14:04:59 +0800

    update loader.go

--- loader/loader_test.go
+++ loader/loader_test.go
@@ -4,10 +4,24 @@ import (
 	"testing"
 )
 
+type RedisCfg struct {
+	Ip   string
+	Port int
+}
+
+type TestCfg struct {
+	Name    string
+	Enabled bool
+	Qq      int
+	Numbers []int
+	Email   []string
+	Redis   RedisCfg
+}
+
 func Test_run(t *testing.T) {
-	ch := make(chan bool, 1)
-	ch <- true
-	loader := new(Loader)
-	loader.OnInit()
-	loader.Run(ch)
+	data := new(TestCfg)
+	Loader("./test.json", data)
+	if data.Email[1] != "gfandada@gmail.com" {
+		t.Error("loader error")
+	}
 }
