e196837edba65b4d03388741895003e8413d295a
gfandada <gfandada@gmail.com>
2017-07-11 10:21:24 +0800

    update db_redis

---
+++ db/db_redis.go
@@ -0,0 +1,212 @@
+package db
+
+import (
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+/******
+type Pool struct {
+    // 用来创建redis连接的方法
+    Dial func() (Conn, error)
+    // 如果设置了给func,那么每次p.Get()的时候都会调用该方法来验证连接的可用性
+    TestOnBorrow func(c Conn, t time.Time) error
+    // 定义连接池中最大连接数（超过这个数会关闭老的链接，总会保持这个数）
+    MaxIdle int
+    // 当前连接池中可用的链接数
+    MaxActive int
+    // 定义链接的超时时间，每次p.Get()的时候会检测这个连接是否超时（超时会关闭，并释放可用连接数）
+    IdleTimeout time.Duration
+    // 当可用连接数为0是，那么当wait=true,那么当调用p.Get()时，会阻塞等待，否则，返回nil.
+    Wait bool
+}
+******/
+
+const (
+	IMAGEKEY = "GSERVERKEY"
+)
+
+type Cache struct {
+	pool *redis.Pool // 连接池
+	key  string      // 用于记录redis中所有的key
+}
+
+type Redis struct {
+	MaxIdle            int
+	MaxActive          int
+	IdleTimeout        int
+	RedisServer        string
+	DialConnectTimeout int
+	DialReadTimeout    int
+	DialWriteTimeout   int
+	Auth               string
+	DbNum              int
+}
+
+// 新建redis-pool
+func NewRedis(redisCfg Redis) *Cache {
+	cache := &Cache{
+		key: IMAGEKEY,
+	}
+	cache.initRedis(redisCfg)
+	conn := cache.pool.Get()
+	defer conn.Close()
+	return cache
+}
+
+func (cache *Cache) initRedis(redisCfg Redis) {
+	dialFunc := func() (c redis.Conn, err error) {
+		c, err = redis.Dial("tcp", redisCfg.RedisServer)
+		if err != nil {
+			return nil, err
+		}
+		if redisCfg.Auth != "" {
+			if _, err := c.Do("AUTH", redisCfg.Auth); err != nil {
+				c.Close()
+				return nil, err
+			}
+		}
+		_, selecterr := c.Do("SELECT", redisCfg.DbNum)
+		if selecterr != nil {
+			c.Close()
+			return nil, selecterr
+		}
+		return
+	}
+	var maxIdle, maxActive int
+	var idleTimeout time.Duration
+	if redisCfg.MaxIdle <= 0 {
+		maxIdle = 3
+	} else {
+		maxIdle = redisCfg.MaxIdle
+	}
+	if redisCfg.MaxActive <= 0 {
+		maxActive = 32
+	} else {
+		maxActive = redisCfg.MaxActive
+	}
+	if redisCfg.IdleTimeout <= 0 {
+		idleTimeout = time.Duration(180) * time.Second
+	} else {
+		idleTimeout = time.Duration(redisCfg.IdleTimeout) * time.Second
+	}
+	cache.pool = &redis.Pool{
+		MaxIdle:     maxIdle,
+		MaxActive:   maxActive,
+		IdleTimeout: idleTimeout,
+		Dial:        dialFunc,
+	}
+}
+
+/*******************************封装调用接口*******************************/
+
+// 执行redis命令
+func (cache *Cache) do(commandName string, args ...interface{}) (reply interface{}, err error) {
+	conn := cache.pool.Get()
+	defer conn.Close()
+	return conn.Do(commandName, args...)
+}
+
+// 获取指定key
+func (cache *Cache) Get(key string) interface{} {
+	if v, err := cache.do("GET", key); err == nil {
+		return v
+	}
+	return nil
+}
+
+// 获取多个key
+func (cache *Cache) GetMulti(keys []string) []interface{} {
+	size := len(keys)
+	var rv []interface{}
+	conn := cache.pool.Get()
+	defer conn.Close()
+	var err error
+	for _, key := range keys {
+		err = conn.Send("GET", key)
+		if err != nil {
+			goto ERROR
+		}
+	}
+	if err = conn.Flush(); err != nil {
+		goto ERROR
+	}
+	for i := 0; i < size; i++ {
+		if v, err := conn.Receive(); err == nil {
+			rv = append(rv, v.([]byte))
+		} else {
+			rv = append(rv, err)
+		}
+	}
+	return rv
+ERROR:
+	rv = rv[0:0]
+	for i := 0; i < size; i++ {
+		rv = append(rv, nil)
+	}
+	return rv
+}
+
+// 存储一对k-v
+func (cache *Cache) Put(key string, val interface{}, timeout time.Duration) error {
+	var err error
+	if _, err = cache.do("SETEX", key, int64(timeout/time.Second), val); err != nil {
+		return err
+	}
+	if _, err = cache.do("HSET", cache.key, key, true); err != nil {
+		return err
+	}
+	return err
+}
+
+// 删除指定key
+func (cache *Cache) Delete(key string) error {
+	var err error
+	if _, err = cache.do("DEL", key); err != nil {
+		return err
+	}
+	_, err = cache.do("HDEL", cache.key, key)
+	return err
+}
+
+// 检查指定key是否存在
+func (cache *Cache) IsExist(key string) bool {
+	v, err := redis.Bool(cache.do("EXISTS", key))
+	if err != nil {
+		return false
+	}
+	if !v {
+		if _, err = cache.do("HDEL", cache.key, key); err != nil {
+			return false
+		}
+	}
+	return v
+}
+
+// 自增指定key
+func (cache *Cache) Incr(key string) error {
+	_, err := redis.Bool(cache.do("INCRBY", key, 1))
+	return err
+}
+
+// 自减指定key
+func (cache *Cache) Decr(key string) error {
+	_, err := redis.Bool(cache.do("INCRBY", key, -1))
+	return err
+}
+
+// 清理所有缓存
+func (cache *Cache) ClearAll() error {
+	cachedKeys, err := redis.Strings(cache.do("HKEYS", cache.key))
+	if err != nil {
+		return err
+	}
+	for _, str := range cachedKeys {
+		if _, err = cache.do("DEL", str); err != nil {
+			return err
+		}
+	}
+	_, err = cache.do("DEL", cache.key)
+	return err
+}
---
+++ db/db_redis_test.go
@@ -0,0 +1,149 @@
+package db
+
+import (
+	"testing"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+//func Test_db(t *testing.T) {
+//	NewDbPool(Redis{
+//		MaxIdle:            8,
+//		MaxActive:          64,
+//		IdleTimeout:        300,
+//		RedisServer:        "192.168.78.130:6379",
+//		DialConnectTimeout: 3,
+//		DialReadTimeout:    3,
+//		DialWriteTimeout:   3,
+//		Auth:               "",
+//	})
+//	// set单值
+//	_, err := Exec("SET", "email", "gfandada@gmail.com")
+//	if err != nil {
+//		t.Errorf("set error")
+//	}
+//	value, err1 := redis.String(Exec("GET", "email"))
+//	if err1 != nil || value != "gfandada@gmail.com" {
+//		t.Errorf("get error")
+//	}
+//	_, err = Exec("SET", "qq", 1009310068)
+//	if err != nil {
+//		t.Errorf("set error")
+//	}
+//	value2, err2 := redis.Int(Exec("GET", "qq"))
+//	if err2 != nil || value2 != 1009310068 {
+//		t.Errorf("get error")
+//	}
+//	_, err = Exec("SET", "github", "https://github.com/gfandada")
+//	if err != nil {
+//		t.Errorf("set error")
+//	}
+//	// 适用于排行榜的有序set
+//	// 有序Set，支持每个键值（比如玩家id）拥有一个分数（score），每次往这个set里添加元素，
+//	// Redis会对其进行排序，修改某一元素的score后，也会更新排序，在获取数据时，可以指定排序范围。
+//	// 更重要的是，这个排序结果会被保存起来，不用在服务器启动时重新计算。
+//	Exec("ZADD", "test1", "120", "user1", "122", "user3")
+//	value3, err3 := redis.Strings(Exec("ZRANGE", "test1", "0", "1111111"))
+//	if err3 != nil || len(value3) != 2 || value3[0] != "user1" || value3[1] != "user3" {
+//		t.Errorf("ZRANGE error")
+//	}
+//	// 跨服（消息队列）
+//	// Redis提供的List数据类型，可以用来实现一个消息队列。
+//	// 由于它是独立于游戏服务器的，所以多个游戏服务器可以通过它来交换数据、发送事件。
+//	// Redis还提供了发布、订阅的事件模型。
+//	// 利用这些，我们就不必自己去实现一套服务器间的通信框架，方便地实现服务器组。
+//	_, err = Exec("LPUSH", "test123", "a", "b", "c")
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	values, errs := redis.Strings(Exec("BLPOP", "test123", 1000))
+//	fmt.Println(values, errs)
+//	if errs != nil {
+//		t.Error(errs)
+//	}
+//	// 自增id
+//	Exec("SET", "userid", "10000")
+//	Exec("INCR", "userid")
+//	Exec("GET", "userid")
+//}
+
+func Test_redis(t *testing.T) {
+	redisCfg := Redis{
+		MaxIdle:            8,
+		MaxActive:          64,
+		IdleTimeout:        300,
+		RedisServer:        "192.168.78.130:6379",
+		DialConnectTimeout: 3,
+		DialReadTimeout:    3,
+		DialWriteTimeout:   3,
+		Auth:               "",
+		DbNum:              3,
+	}
+	bm := NewRedis(redisCfg)
+	timeoutDuration := 10 * time.Second
+	var err error
+	if err = bm.Put("gfandada", 1, timeoutDuration); err != nil {
+		t.Error("set Error", err)
+	}
+	if !bm.IsExist("gfandada") {
+		t.Error("check err")
+	}
+	time.Sleep(11 * time.Second)
+	if bm.IsExist("gfandada") {
+		t.Error("check err")
+	}
+	if err = bm.Put("gfandada", 1, timeoutDuration); err != nil {
+		t.Error("set Error", err)
+	}
+	if v, _ := redis.Int(bm.Get("gfandada"), err); v != 1 {
+		t.Error("get err")
+	}
+	if err = bm.Incr("gfandada"); err != nil {
+		t.Error("Incr Error", err)
+	}
+	if v, _ := redis.Int(bm.Get("gfandada"), err); v != 2 {
+		t.Error("get err")
+	}
+	if err = bm.Decr("gfandada"); err != nil {
+		t.Error("Decr Error", err)
+	}
+	if v, _ := redis.Int(bm.Get("gfandada"), err); v != 1 {
+		t.Error("get err")
+	}
+	bm.Delete("gfandada")
+	if bm.IsExist("gfandada") {
+		t.Error("delete err")
+	}
+	//test string
+	if err = bm.Put("gfandada", "author", timeoutDuration); err != nil {
+		t.Error("set Error", err)
+	}
+	if !bm.IsExist("gfandada") {
+		t.Error("check err")
+	}
+	if v, _ := redis.String(bm.Get("gfandada"), err); v != "author" {
+		t.Error("get err")
+	}
+	//test GetMulti
+	if err = bm.Put("gfandada1", "author1", timeoutDuration); err != nil {
+		t.Error("set Error", err)
+	}
+	if !bm.IsExist("gfandada1") {
+		t.Error("check err")
+	}
+	vv := bm.GetMulti([]string{"gfandada", "gfandada1"})
+	if len(vv) != 2 {
+		t.Error("GetMulti ERROR")
+	}
+	if v, _ := redis.String(vv[0], nil); v != "author" {
+		t.Error("GetMulti ERROR")
+	}
+	if v, _ := redis.String(vv[1], nil); v != "author1" {
+		t.Error("GetMulti ERROR")
+	}
+	// test clear all
+	//	if err = bm.ClearAll(); err != nil {
+	//		t.Error("clear all err")
+	//	}
+}
