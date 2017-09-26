5ff5137c43f17cf0c8d28ddf7c762631c74964b2
gfandada <gfandada@gmail.com>
2017-09-26 10:15:51 +0800

    delete info

--- network/msgparser.go
+++ network/msgparser.go
@@ -1,25 +1,6 @@
 // 报文解析模块
 package network
 
-/*
-	client->gateway
-	----------------------------
-	| len | seq | id | message |
-	----------------------------
-	len:seq + id + message的长度，占用2个字节(uint16)
-	seq:从1自增的序列号，占用4个字节(uint32)
-	id:协议号，占用2个字节(uint16)
-	message:业务数据，占用len-4-2字节，可以使用任意编码：pb/json等
-
-	gateway->client
-	----------------------
-	| len | id | message |
-	----------------------
-	len:message的长度，占用2个字节(uint16)
-	id:协议号，占用两个字节(uint16)
-	message:业务数据，占用len-2字节，可以使用任意编码：pb/json等
-*/
-
 import (
 	"encoding/binary"
 	"errors"
